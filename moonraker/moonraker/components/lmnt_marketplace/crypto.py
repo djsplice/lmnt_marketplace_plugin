55e8bea13b72b98327fe7e26d6f386300f202838
djsplice <barrows.jeff@gmail.com>
2025-06-10 18:15:56 -0700

    dek fun

--- moonraker/moonraker/components/lmnt_marketplace/crypto.py
+++ moonraker/moonraker/components/lmnt_marketplace/crypto.py
@@ -94,6 +94,11 @@ class CryptoManager:
             bytes: Decrypted DEK if successful
             None: If decryption failed
         """
+        # In development mode, use a fixed DEK for testing
+        if self.integration.development_mode:
+            logging.info("Using development mode fixed DEK")
+            # Return a fixed 32-byte key for development mode
+            return b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f'
         if not encrypted_dek_hex:
             logging.error("Cannot decrypt empty DEK")
             return None
